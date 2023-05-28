606db7f5754fcd003a206af70039cab85bdb4d67
Abena konadu <abenagyamfi99@gmail.com>
2023-05-28 15:53:52 +0000

    Components and services created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { SettingsComponent } from './components/admin/settings/settings.component';
+import { TablesComponent } from './components/admin/tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'reset-password',component:ResetPasswordComponent},
+  {path:'forgot-password',component:ForgotPasswordComponent},
+  {path:'settings',component:SettingsComponent},
+  {path:'tables',component:TablesComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'**',redirectTo:'dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { SettingsComponent } from './components/admin/settings/settings.component';
+import { TablesComponent } from './components/admin/tables/tables.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { HeaderComponent } from './views/header/header.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { SidebarComponent } from './views/sidebar/sidebar.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SettingsComponent,
+    TablesComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    RegisterComponent,
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+
+ }
